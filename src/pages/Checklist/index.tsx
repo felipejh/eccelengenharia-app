e148033224ab3d96e4f413a2facd3a70d664332c
Felipe Hoffmann <felipejh@gmail.com>
2021-10-21 22:37:50 -0300

    feat: improve list layout

--- src/pages/Checklist/index.tsx
+++ src/pages/Checklist/index.tsx
@@ -2,12 +2,25 @@ import React, { FC, useState, useEffect } from 'react';
 import { Text } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 import { AxiosResponse } from 'axios';
+import { parseISO, isAfter } from 'date-fns';
 import { Checklist, ChecklistScreenRouteProp } from '~/models/checklist.model';
 import { Plan } from '~/models/plans.model';
 import api from '~/services/api';
 import LoadingModal from '~/components/LoadingModal';
 
-import { Container, List, ContainerList } from './styles';
+import {
+  Container,
+  List,
+  ContainerList,
+  TextItemConferir,
+  ContainerTolerancia,
+  TextToleranciaLabel,
+  TextToleranciaValue,
+  ContainerStatus,
+  TextStatusLabel,
+  TextStatusValue,
+} from './styles';
+import colors from '~/styles/colors';
 
 const ChecklistScreen: FC = () => {
   const route = useRoute<ChecklistScreenRouteProp>();
@@ -26,8 +39,76 @@ const ChecklistScreen: FC = () => {
         );
 
         const { data } = response;
-        setListChecklists(data.checklist);
-        console.tron.log(data.checklist);
+        // data.checklist.push(data.checklist.map(d => ({ ...d })));
+        // data.checklist.push(data.checklist.map(d => ({ ...d })));
+        // data.checklist.push(data.checklist.map(d => ({ ...d })));
+
+        // console.tron.log(data.checklist);
+        const test = data.checklist.map(a => {
+          const c = [
+            {
+              id: 2,
+              situacao: 'APROVADO',
+              dth_resposta: '2021-08-28T08:00:00.003Z',
+              createdAt: '2021-09-18T13:26:39.214Z',
+              updatedAt: '2021-09-18T13:26:39.214Z',
+              checklistId: 2,
+              plantaId: 1,
+              ocorrenciaId: null,
+              usuarioCreateId: 3,
+              usuarioUpdateId: null,
+              usuarioUpdate: null,
+              usuarioCreate: {
+                id: 3,
+                usuario: 'felipe',
+                nome: 'felipe',
+              },
+            },
+            {
+              id: 1,
+              situacao: 'REPROVADO',
+              dth_resposta: '2021-07-28T08:00:00.003Z',
+              createdAt: '2021-09-18T13:26:39.214Z',
+              updatedAt: '2021-09-18T13:26:39.214Z',
+              checklistId: 2,
+              plantaId: 1,
+              ocorrenciaId: null,
+              usuarioCreateId: 3,
+              usuarioUpdateId: null,
+              usuarioCreate: {
+                id: 3,
+                usuario: 'felipe',
+                nome: 'felipe',
+              },
+              usuarioUpdate: null,
+            },
+          ];
+
+          return {
+            ...a,
+            answers: c,
+          };
+        });
+
+        const dataSorted: Array<Checklist> = test.map(checklist => {
+          const answersSorted = checklist.answers?.sort((a, b) => {
+            const dateA = parseISO(a.dth_resposta);
+            const dateB = parseISO(b.dth_resposta);
+
+            if (isAfter(dateA, dateB)) {
+              return -1;
+            }
+            return 0;
+          });
+
+          return {
+            ...checklist,
+            answers: answersSorted,
+          };
+        });
+
+        setListChecklists(dataSorted);
+        console.tron.log(dataSorted);
         setLoading(false);
       } catch {
         setLoading(false);
@@ -39,22 +120,42 @@ const ChecklistScreen: FC = () => {
 
   return (
     <Container>
-      <LoadingModal loading={loading} text="Buscando checklists" />
-
       <List
         data={listChecklists}
         keyExtractor={(checklist: Checklist) => String(checklist.id)}
         renderItem={({ item }) => {
-          // console.tron.log(item);
+          let status = item.answers?.[0].situacao;
+
+          let statusColor =
+            item.answers?.[0].situacao === 'APROVADO'
+              ? colors.green
+              : colors.red;
+
+          if (!item.answers?.[0].situacao) {
+            status = 'VERIFICAR';
+            statusColor = colors.yellow;
+          }
+          status = 'VERIFICAR';
+          statusColor = colors.gray_light;
+
           return (
             <ContainerList>
-              <Text>{item.item_conferir}</Text>
-              <Text>{item.tolerancia}</Text>
-              <Text>{item.answers?.[0].situacao}</Text>
+              <TextItemConferir>{item.item_conferir}</TextItemConferir>
+              <ContainerTolerancia>
+                <TextToleranciaLabel>Tolerância: </TextToleranciaLabel>
+                <TextToleranciaValue>{item.tolerancia}</TextToleranciaValue>
+              </ContainerTolerancia>
+
+              <ContainerStatus>
+                <TextStatusLabel>Situação: </TextStatusLabel>
+                <TextStatusValue color={statusColor}>{status}</TextStatusValue>
+              </ContainerStatus>
             </ContainerList>
           );
         }}
       />
+
+      <LoadingModal loading={loading} text="Buscando checklists" />
     </Container>
   );
 };
--- src/pages/Checklist/styles.ts
+++ src/pages/Checklist/styles.ts
@@ -1,8 +1,13 @@
 import { FlatList } from 'react-native';
 import styled from 'styled-components/native';
 import { Checklist } from '~/models/checklist.model';
+import colors from '~/styles/colors';
+import fonts from '~/styles/fonts';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  background: ${colors.black_strong};
+`;
 
 export const List = styled(FlatList as new () => FlatList<Checklist>).attrs({
   contentContainerStyle: {
@@ -10,4 +15,49 @@ export const List = styled(FlatList as new () => FlatList<Checklist>).attrs({
   },
 })``;
 
-export const ContainerList = styled.View``;
+export const ContainerList = styled.View`
+  border-radius: 4px;
+  padding: 15px;
+  elevation: 4;
+  shadow-opacity: 0.75;
+  shadow-radius: 5px;
+  shadow-color: ${colors.black_strong};
+  shadow-offset: 0px 0px;
+  margin: 10px 10px 0;
+  background: ${colors.white};
+`;
+
+export const TextItemConferir = styled.Text`
+  font-family: ${fonts.heading};
+  color: ${colors.black_light};
+  font-size: 18px;
+`;
+
+export const ContainerTolerancia = styled.View`
+  flex-direction: row;
+`;
+
+export const TextToleranciaLabel = styled.Text`
+  font-family: ${fonts.heading};
+  color: ${colors.black_light};
+`;
+
+export const TextToleranciaValue = styled.Text`
+  font-family: ${fonts.text};
+  color: ${colors.black_light};
+`;
+
+export const ContainerStatus = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const TextStatusLabel = styled.Text`
+  font-family: ${fonts.heading};
+  color: ${colors.black_light};
+`;
+
+export const TextStatusValue = styled.Text<{ color: string }>`
+  font-family: ${fonts.heading};
+  color: ${props => props.color};
+`;
